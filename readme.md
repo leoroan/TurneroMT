e11e9821b63a4dfe4929d3e763d065761fcde7af
Leandro Maselli <leoroan@gmail.com>
2024-11-07 16:39:39 -0300

    nueva edit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# TurneroMT@2024
+
+- Empaquetado con PKG
+- command: pkg . --targets node18-win-x64 --output turneroMt.exe
--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,10 +1,20 @@
 // import { Sequelize } from 'sequelize'
 const { Sequelize } = require('sequelize')
+const path = require('path');
 
+if (!process.env.USERPROFILE) {
+  throw new Error('No se puede obtener la ruta del perfil del usuario.');
+}
+// Obtener la ruta al directorio del escritorio del usuario en Windows
+const desktopPath = path.join(process.env.USERPROFILE, 'Desktop');
+
+// Definir el nombre y la ruta para la base de datos
+const dbPath = path.join(desktopPath, 't-mt-bdd.sqlite');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database/database.sqlite',
+  storage: dbPath,
+  // storage: './database/database.sqlite',
   logging: false
 })
 
